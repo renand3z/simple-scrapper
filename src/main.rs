b4ce2ddabebf2fdfff9a55992d3ee736754546d9
renan <renand3z@gmail.com>
2023-08-22 02:29:52 +0200

    working sometimes

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#[tokio::main]
+async fn main() {
+    let resp = reqwest::get("https://wwwg.example.com").await.unwrap();
+    assert!(resp.status().is_success());
+
+    let body = resp.text().await.unwrap();
+
+    let fragment = scraper::Html::parse_document(&body);
+    let selector = scraper::Selector::parse("a[href]").unwrap();
+    for element in fragment.select(&selector) {
+        let href = element.value().attr("href").unwrap();
+        println!("{:?}", href);
+    }
+}
